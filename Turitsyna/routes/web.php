781384c9f7c1fdf2048ccdda1fd14ad382ce3140
DmitriiSasov <s-dima29@yandex.ru>
2021-01-15 12:00:02 +0300

    Частично обновлены маршруты.

--- Turitsyna/routes/web.php
+++ Turitsyna/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Group;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,3 +33,60 @@ Route::get('/db', function () {
         return $group;
     }
 });
+
+/**
+ * Добавить нового студента в группу
+ * */
+Route::post('/enrollment/add', function ($student, $group) {
+
+});
+
+/**
+ * Удалить студента из группы
+ * */
+Route::delete('/enrollment/delete/{groupId}/{studentId}', function ($groupId, $studentId) {
+
+});
+
+/**
+ * Переместить студента из одной группы в другую
+ * */
+Route::patch('/enrollment/changeStudentGroup/{studentId}', function ($studentId, $prevGroupId, $newGroup) {
+
+});
+
+/**
+ * Изменить название группы
+ * */
+Route::patch('/enrollment/changeGroupName/{groupId}', function ($groupId, $newGroupName) {
+
+});
+
+/**
+ * Получить студентов по группам согласен фильтру
+ */
+Route::get('/getGroupStudents', function ($date, $course, $spec, $groupName) {
+
+});
+
+/**
+ * Получить студентов, которых зачисляют на 1 курс
+ */
+Route::get('/enrollment/getGroupStudents', function ($spec, $groupName) {
+
+});
+
+/**
+ * Получить предварительные составы групп, которые будут сформированы после перевода
+ */
+Route::get('/transfer/getNewStudentsGroups', function () {
+
+});
+
+/**
+ * Перевести студентов на старший курс
+ */
+Route::patch('/transfer', function () {
+
+});
+
