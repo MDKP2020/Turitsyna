8a5700f56286f9d86afb3b7e8347de11e11074f6
DmitriiSasov <s-dima29@yandex.ru>
2021-01-19 13:27:42 +0300

    Добавлены тесты на удаление студента. Теперь при запуске тесты оборачиваются в транзакции.

--- Turitsyna/tests/Feature/AddStudentsTest.php
+++ Turitsyna/tests/Feature/AddStudentsTest.php
@@ -2,22 +2,105 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AddStudentsTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
-     * A basic feature test example.
+     * Тест на добавление студента без данных о студенте
      *
      * @return void
      */
-    public function testExample()
+    public function testEmptyRequest()
     {
-        
-        $response = $this->post('/enrollment/add', );
+        $response = $this->post('/enrollment-api/addStudent', [], []);
 
-        $response->assertStatus(200);
+        $response->assertStatus(400);
+        $response->assertJson(['Not enough information']);
+    }
+
+    /**
+     * Тест на добавление студента
+     *
+     * @return void
+     */
+    public function testCorrectStudent()
+    {
+        $response = $this->post('/enrollment-api/addStudent', [
+            'name' => 'Виталий',
+            'surname' => 'Витальев',
+            'patronomyc' => 'Витальевич',
+            'group_id' => 1
+        ], []);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('student', ['name' => 'Виталий',
+            'surname' => 'Витальев',
+            'patronomyc' => 'Витальевич']);
+
+    }
+
+    /**
+     * Тест на добавление студента повторно
+     *
+     * @return void
+     */
+    public function testAddingToIdenticalStudents()
+    {
+        $response = $this->post('/enrollment-api/addStudent', [
+            'name' => 'Иван',
+            'surname' => 'Иванов',
+            'patronomyc' => 'Иванович',
+            'group_id' => 1
+        ], []);
+        $response->assertStatus(400);
+        $response->assertJson(['Student is studying']);
+    }
+
+    /**
+     * Тест на удаление студента
+     *
+     * @return void
+     */
+    public function testDeleteStudent()
+    {
+
+        $response = $this->delete('/enrollment-api/delStudFromGroup/14/1', [], []);
+
+        $this->assertDatabaseMissing('student_group', [
+            "student_id" => 14,
+            'group_id' => 1
+        ],
+        );
+        /*$response->assertStatus(400);
+        $response->assertJson(['Student is studying']);*/
+    }
+
+    /**
+     * Тест на удаление несуществующего студента
+     *
+     * @return void
+     */
+    public function testDeleteUnknownStudent()
+    {
+
+        $response = $this->delete('/enrollment-api/delStudFromGroup/15/1', [], []);
+
+        $response->assertStatus(500);
+    }
+
+    /**
+     * Тест на удаление студента по нулевым данным
+     *
+     * @return void
+     */
+    public function testDeleteStudentWithoutData()
+    {
+        $response = $this->delete('/enrollment-api/delStudFromGroup/null/null', [], []);
+
+        $response->assertStatus(500);
     }
 }
