f4cee79d857ae3392a67450da2b104cbd016345a
DmitriiSasov <s-dima29@yandex.ru>
2021-01-19 16:46:53 +0300

    Добавлены тесты на взаимодействие со студентами.

---
+++ Turitsyna/tests/Feature/StudentTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StudentTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * Получить полную информацию о студенте
+     *
+     * @return void
+     */
+    public function testGetStudentInfo()
+    {
+        $response = $this->get('/student-api/getStudent/', [
+            'name' => 'Иван',
+            'surname' => 'Иванов',
+            'patronomyc' => 'Иванович',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'id' => 1,
+            'name' => 'Иван',
+            'surname' => 'Иванов',
+            'patronomyc' => 'Иванович']);
+    }
+
+    /**
+     * Получить полную информацию о несуществующем студенте
+     *
+     * @return void
+     */
+    public function testGetUnknownStudentInfo()
+    {
+        $response = $this->get('/student-api/getStudent/', [
+            'name' => 'Иван1',
+            'surname' => 'Иванов1',
+            'patronomyc' => 'Иванович1',
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson(['Not enough information']);
+    }
+
+    /**
+     * Получить студентов из группы
+     *
+     * @return void
+     */
+    public function testGetStudentsFromGroup()
+    {
+        $response = $this->get('/student-api/getStudentsFromGroup/1',[]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'name' => 'PRIN-166',
+            'students' => [
+                [
+                    'id' => 1,
+                    'name' => 'Иван',
+                    'surname' => 'Иванов',
+                    'patronomyc' => 'Иванович',
+                    "created_at" => null,
+                    "updated_at" => null
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * Получить студентов из несуществующей группы
+     *
+     * @return void
+     */
+    public function testGetStudentsFromUnknownGroup()
+    {
+        $response = $this->get('/student-api/getStudentsFromGroup/6',[]);
+
+        $response->assertStatus(500);
+    }
+}
